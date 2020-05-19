6a4178575dbcc50a1549f899e396ee6ac606998a
Juan <jbcarazo@gmail.com>
2020-05-19 15:14:57 +0200

    solved morpher problems

--- implementation/sparse/morpher.py
+++ implementation/sparse/morpher.py
@@ -17,9 +17,9 @@ class Morpher(object):
             params = experiment.arms_by_name[arm].parameters
             self.configs[arm] = params
 
-    def apply_morphs(self, n_morphs=5, max_n_changes=10):
+    def apply_morphs(self, n_morphs=5, max_n_changes=100):
         configs_sub = choice(list(self.configs), size=n_morphs)
-        n_changes_x_morph = randint(1, max_n_changes, size=n_morphs)
+        n_changes_x_morph = randint(10, max_n_changes, size=n_morphs)
         new_configs = {}
         for i, (config, changes) in enumerate(zip(configs_sub, n_changes_x_morph)):
             ops = choice(list(self.operations), changes)
