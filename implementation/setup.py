d2cba5a780b7506d80c9683dfd26ebb34d4a83af
Juan <jbcarazo@gmail.com>
2020-05-29 18:31:54 +0200

    added setup for installing sparse as a package

---
+++ implementation/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='sparse',
+version='0.1',
+description='Sparse Package installation',
+url='#',
+author='BCJuan',
+author_email='bcjuan@protonmail.com',
+license='MIT',
+packages=['sparse'],
+zip_safe=False)
