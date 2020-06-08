64b6945330fba295c3ad7d66fe9a9fee66aa23ea
Juan <jbcarazo@gmail.com>
2020-06-08 11:13:58 +0200

    added setup and license files for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sparsemod", # Replace with your own username
+    version="0.0.1",
+    author="Juan Borrego Carazo",
+    author_email="juan.borrego@uab.cat",
+    description="Package for automatically creating performing but low sized neural networks suitable for microcontrollers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BCJuan/SpArSeMod",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
